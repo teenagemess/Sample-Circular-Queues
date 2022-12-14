40bc3a4a02fc0b1226e9399450c5c59f33be5639
Raynanda Aqiyas <raynanda.aqiyas.ft21@mail.umy.ac.id>
2022-12-14 16:56:20 +0700

    membuat function remove

--- Sample Circular Queues/Program.cs
+++ Sample Circular Queues/Program.cs
@@ -39,5 +39,29 @@ namespace CircularQueues_CSharp
             queue_array[REAR] = element;
         }
         public void remove()
+        {
+            //Checks whether the queue is empty
+            if (FRONT == -1)
+            {
+                Console.WriteLine("Queue underflow\n");
+                return;
+            }
+            Console.WriteLine("\nThe element deleted from the queue is: " + queue_array[FRONT] + "\n");
+            // Checks if the queue has one element
+            if (FRONT == REAR)
+            {
+                FRONT = -1;
+                REAR = -1;
+            }
+            else
+            {
+                //if the element to be deleted is at the lasst position of the array, then the value of FRONT is set to 0 i.e to the first element of the array
+                if (FRONT == max - 1)
+                    FRONT = 0;
+                else
+                    //Front is incremented by one if it is not the first element of array
+                    FRONT = FRONT + 1;
+            }
+        }
     }
 }
