d8def03fc243dca83687d52eb2e717a94ed8a32b
Raynanda Aqiyas <raynanda.aqiyas.ft21@mail.umy.ac.id>
2022-12-14 16:47:13 +0700

    membuat class Queues dan function insert

--- Sample Circular Queues/Program.cs
+++ Sample Circular Queues/Program.cs
@@ -3,4 +3,20 @@
 namespace CircularQueues_CSharp
 {
     class Queues
+    {
+        int FRONT, REAR, max = 5;
+        int[] queue_array = new int[5];
+        public Queues()
+        {
+            //Initializing the values of the variables REAR and FRONT to indicate that the queue is initially empty
+            FRONT = -1;
+            REAR = -1;
+        }
+        public void insert(int element)
+        {
+            // This statement cchecks for the overflow condition.
+            if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1))
+
+        }
+    }
 }
