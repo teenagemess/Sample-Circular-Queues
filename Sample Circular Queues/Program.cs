f28d121de711d2a38f3e487626f9a46cf27ec54d
Raynanda Aqiyas <raynanda.aqiyas.ft21@mail.umy.ac.id>
2022-12-14 17:03:25 +0700

    membuat function display

--- Sample Circular Queues/Program.cs
+++ Sample Circular Queues/Program.cs
@@ -63,5 +63,46 @@ namespace CircularQueues_CSharp
                     FRONT = FRONT + 1;
             }
         }
+        public void display()
+        {
+            int FRONT_position = FRONT;
+            int REAR_position = REAR;
+            //Checks if the queue is empty
+            if (FRONT == -1)
+            {
+                Console.WriteLine("Queue is empty\n");
+                return;
+            }
+            Console.WriteLine("\nElements in the queue are................\n");
+            if (FRONT_position <= REAR_position)
+            {
+                //Traverse the queue till the last element present in an array
+                while (FRONT_position <= REAR_position)
+                {
+                    Console.Write(queue_array[FRONT_position] + "   ");
+                    FRONT_position++;
+                }
+                Console.WriteLine();
+            }
+            else
+            {
+                //traverses the queue till the last position of the array
+                while (FRONT_position <= max - 1)
+                {
+                    Console.WriteLine(queue_array[FRONT_position] + "    ");
+                    FRONT_position++;
+                }
+                //set the FRONT position to the first element of the array
+                FRONT_position = 0;
+                //traverses the array till the last element present in the queue
+                while(FRONT_position <= REAR_position)
+                {
+                    Console.Write(queue_array[FRONT_position] + "    ");
+                    FRONT_position++;
+                }
+                Console.WriteLine();
+            }
+        }
+
     }
 }
